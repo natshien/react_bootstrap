e7bcb11c6bc883143a5831aa7b82e5a45c4d3f9f
Natalia Walendzik <Natalia_Walendzik@epam.com>
2020-03-02 15:48:32 +0100

    checking options

--- my-app/src/components/AnswerQuestions.js
+++ my-app/src/components/AnswerQuestions.js
@@ -1,40 +1,44 @@
 /* Selecting up to 5 questions from selected by user category, user clicks one of two answers and goes to next question.
 After last question is answered it should show the button 'see comparison'
  */
-
+ 
 import React, { useState, useEffect } from 'react';
-import { questionsDB, answersDB } from '../mockupDB.js/questionsDB';
+import { questionsDB } from '../mockupDB.js/questionsDB';
+//import SlideQuestion from './SlideQuestion';
 
 
-const AnswerQuestions = (props) => {
-  
-  const [questions, setQuestions] = useState(props);
-  
+
+const AnswerQuestions = () => {
+ 
+ const [questions, setQuestions] = useState([]);
+ const [index, setIndex] = useState(0);
+
   useEffect(()=>{
     setQuestions(questionsDB);
   }, []);
   
   console.log(questions);
 
-  return (
-    <>
-    HELLO REACT
-    </>
+  const getMap = () => questions.map((question, index) => {
+    return (
+    <div className="askBox" index={question[index]}>
+        <h5>{question.questionText}</h5>
+        <form>
+          <input type="radio" id={"ansA" + index} name="answer" value={question.answerA}></input> <label for="ansA">{question.answerA}</label><br />
+          <input type="radio" id={"ansB" + index}  name="answer" value={question.answerB}></input> <label for="ansB">{question.answerB}</label><br />
+          <button>Prev</button>
+          <button type="submit">Next</button>
+        </form>
+    </div>
+    )});
+ 
+  return  (
+  <div className="questions">
+    {getMap()}
+  </div>
   )
-      
+    
 };
-
+ 
+ 
 export default AnswerQuestions;
-
-//questions.map((questions,index) => {
-      //return (<div className="askBox">
-      //  <h5>{question.questionText}</h5>
-      //  <form>
-      //    <input type="radio" id="ansA" name="answer" value={question.answerA}></input> <label for="ansA">{question.answerA}</label><br />
-      //    <input type="radio" id="ansB" name="answer" value={question.answerB}></input> <label for="ansB">{question.answerB}</label><br />
-      //    <button>Prev</button>
-      //    <button type="submit">Next</button>
-      //  </form>
-      //</div>
-      //)
-    //});
---
+++ my-app/src/components/SlideQuestion.js
@@ -0,0 +1,18 @@
+//import React from 'react';
+//
+//
+//const SlideQuestion = ({questions}) => {
+//    return (
+//        <div className="askBox" index = {question[index]}>
+//        <h5>{question.questionText}</h5>
+//        <form>
+//          <input type="radio" id={"ansA" + index} name="answer" value={question.answerA}></input><label for="ansA">{question.answerA}</label><br />
+//          <input type="radio" id={"ansB" + index}  name="answer" value={question.answerB}></input> <label for="ansB">{question.answerB}</label><br />
+//          <button>Prev</button>
+//          <button type="submit">Next</button>
+//        </form>
+//    </div>
+//    )
+//}
+//
+//export default SlideQuestion;
